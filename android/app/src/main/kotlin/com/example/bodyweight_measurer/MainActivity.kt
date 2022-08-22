8b2de1cd2b5de1ed82f31141cc7978a8fc95e3e9
rajab256 <ssemakularajab25@gmail.com>
2022-08-22 12:08:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bodyweight_measurer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bodyweight_measurer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/right_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:bodyweight_measurer/constants/app_constants.dart';
+
+class RightBar extends StatefulWidget {
+  RightBar({Key? key}) : super(key: key);
+
+  @override
+  State<RightBar> createState() => _RightBarState();
+}
+
+class _RightBarState extends State<RightBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
